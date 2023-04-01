4323de49e2704bfb8aa248e3526ad142f32ec926
Reephoxe <martinviolet@hotmail.com>
2023-04-01 12:02:46 +0200

    debut du projet

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+    // header
+    include("vues/v_header.php");
+
+
+    // footer
+    include("vues/v_footer.php");
+
+?>
---
+++ vues/v_header.php
@@ -0,0 +1,25 @@
+<head>
+    <title>Portfolio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <span class="fs-4">Martin Violet</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link">competence1</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">competence2</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">competence3</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">competence4</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">competence5</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">competence6</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">A Propos</a></li>
+      </ul>
+    </header>
+  </div>
