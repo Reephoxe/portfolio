834fece848daacd6c9dadb87c28f4a2e7b6ac7ae
Reephoxe <martinviolet@hotmail.com>
2023-06-05 19:14:49 +0200

    Fin des documents pour épreuve e5

--- vues/v_e5.php
+++ vues/v_e5.php
@@ -5,12 +5,41 @@
 <?php }else if(($_REQUEST["projet"] == 1)){
 
  ?>
- <h2>Vous êtes bien sur le projet GSB Compte Rendu</h2>
+ <h2>Documentation projet GSB Compte Rendu</h2>
+ <br/>
+<section>
+    <div class="container pt-5">
+    <hr>
+        <p class="h2 text-center">Tests Unitaires</p>
+        <a href="assets/images/tests_unitaires.pdf"><img class="img-fluid pt-5" src="assets/images/tests_unitaires-1.png" alt="tests_unitaires-1"><img class="img-fluid pt-5" src="assets/images/tests_unitaires-2.png" alt="tests_unitaires-2"><img class="img-fluid pt-5" src="assets/images/tests_unitaires-3.png" alt="tests_unitaires-3"></a>
+    </div>
+</section>
+<section>
+    <div class="container pt-5">
+    <hr>
+        <p class="h2 text-center">Tests fonctionnels</p>
+        <a href="assets/images/TestsFonctionnelsMartin.pdf"><img class="img-fluid pt-5" src="assets/images/tests_fonctionnels-5.png" alt="tests_fonctionnels-5"><img class="img-fluid pt-5" src="assets/images/tests_fonctionnels-1.png" alt="tests_fonctionnels-1"><<img class="img-fluid pt-5" src="assets/images/tests_fonctionnels-2.png" alt="tests_fonctionnels-2"><img class="img-fluid pt-5" src="assets/images/tests_fonctionnels-3.png" alt="tests_fonctionnels-3"><img class="img-fluid pt-5" src="assets/images/tests_fonctionnels-4.png" alt="tests_fonctionnels-4">/a>
+    </div>
+</section>
  <?php 
 }else if(($_REQUEST["projet"] == 2)){
  ?>
 
-<h2>Vous êtes bien sur le projet GSB Paramédical</h2>
+<h2>Documentation projet projet GSB Paramédical</h2>
+<section>
+    <div class="container pt-5">
+    <hr>
+        <p class="h2 text-center">Tests Unitaires</p>
+        <a href="assets/images/2tests_unitaires.pdf"><img class="img-fluid pt-5" src="assets/images/2tests_unitaires-1.png" alt="tests_unitaires-1"><img class="img-fluid pt-5" src="assets/images/2tests_unitaires-2.png" alt="tests_unitaires-2"></a>
+    </div>
+</section>
+<section>
+    <div class="container pt-5">
+    <hr>
+        <p class="h2 text-center">Tests fonctionnels</p>
+        <a href="assets/images/lesTestsFonctionnels.pdf"><img class="img-fluid pt-5" src="assets/images/2tests_fonctionnels-1.png" alt="tests_fonctionnels-5"><img class="img-fluid pt-5" src="assets/images/2tests_fonctionnels-5.png" alt="tests_fonctionnels-1"><<img class="img-fluid pt-5" src="assets/images/2tests_fonctionnels-2.png" alt="tests_fonctionnels-2"><img class="img-fluid pt-5" src="assets/images/2tests_fonctionnels-3.png" alt="tests_fonctionnels-3"><img class="img-fluid pt-5" src="assets/images/2tests_fonctionnels-4.png" alt="tests_fonctionnels-4"><img class="img-fluid pt-5" src="assets/images/2tests_fonctionnels-6.png" alt="tests_fonctionnels-4">/a>
+    </div>
+</section>
 
 <?php  }?>
 </div>
